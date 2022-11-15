f69512ed9d5a88ca43b626a59921adda6663df11
Krešimir Divić <kdivus@hotmail.com>
2022-11-15 01:05:48 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import dns from 'dns'
 import laravel from 'laravel-vite-plugin';
 
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
+    server: {
+        fs: {
+            // Allow serving files from one level up to the project root
+            allow: ['..'],
+          },
+      },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
