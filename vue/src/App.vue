81465805ea7e7685b687ecb10f7d4e9e76753571
Krešimir Divić <kdivus@hotmail.com>
2022-11-17 01:19:20 +0100

    setup vue, tailwind, fixing json files

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,27 +3,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import {mapState} from vuex
-
-export default {
-  computed: {
-    ...mapState(['user'])
-  }
-}
 
 </script>
 
 <template>
-  <pre>{{user}}</pre>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> 
 </template>
 
 <style scoped>
